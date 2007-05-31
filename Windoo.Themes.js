381786c38dd038174cf4e64ce0bb24bc5fc8e6d0
ye.gorshkov <ye.gorshkov@b7bedb83-3a2e-0410-a698-db1f1f78d2a9>
2007-05-31 23:18:22 +0000

    - Windoo is now modular! - added onBeforeClose event - onShow, onClose events are now called after the action is performed - misc. fixes in documentation

---
+++ Windoo.Themes.js
@@ -0,0 +1,31 @@
+/*
+Script: Windoo.Themes.js
+	Windoo additional themes.
+	Contains <Windoo.Themes.aero>.
+
+License:
+	MIT-style license.
+
+Copyright:
+	copyright (c) 2007 Yevgen Gorshkov
+*/
+
+/*
+Property: Windoo.Themes.aero
+	Modified 'aero' theme from YUI-Ext library <http://extjs.com/>
+*/
+
+Windoo.Themes.aero = {
+	'name': 'aero',
+	'padding': {'top': 28, 'right': 10, 'bottom': 15, 'left': 10},
+	'resizeLimit': {'x': [175], 'y': [58]},
+	'className': 'windoo windoo-aero',
+	'sizerClass': 'sizer',
+	'classPrefix': 'windoo',
+	'ghostClass': 'windoo-ghost windoo-aero-ghost windoo-hover',
+	'hoverClass': 'windoo-hover',
+	'shadow': 'simple window-aero-shadow-simple',
+	'shadeBackground': 'transparent url(windoo/s.gif)',
+	'shadowDisplace': {'left': 3, 'top': 3, 'width': 0, 'height': 0}
+};
+
