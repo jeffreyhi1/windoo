cfa713b041502fd87cfe8e5827cb01da09a410c0
ye.gorshkov <ye.gorshkov@b7bedb83-3a2e-0410-a698-db1f1f78d2a9>
2007-08-09 10:12:04 +0000

    - fixed previous fix to support 'disabled' button state - title dblclick is attached to a new dom.titleBody conatainer element instead of dom.title - added support for options.initialize

--- src/Windoo/Windoo.Core.js
+++ src/Windoo/Windoo.Core.js
@@ -219,6 +219,7 @@ var Windoo = new Class({
 
 		this.wm = this.options.wm || Windoo.$wm;
 		this.wm.register(this);
+		if (this.options.initialize) this.options.initialize.call(this);
 	},
 
 	/*
@@ -256,12 +257,13 @@ var Windoo = new Class({
 
 		var frame = this.el.getFirst(),
 			body = this.el.getLast(),
-			title = frame.getElement('.title'),
-			titleText = new Element('div', {'class': 'title-text'}).inject(title);
+			titleBody = frame.getElement('.title'),
+			titleText = new Element('div', {'class': 'title-text'}).inject(titleBody);
 		this.dom = {
 			frame: frame,
 			body: body,
 			title: titleText,
+			titleBody: titleBody,
 			strut: frame.getElement('.strut').setHTML('&nbsp;'),
 			content: iefix ? body.getElement('td') : body
 		};
@@ -300,7 +302,7 @@ var Windoo = new Class({
 		};
 		makeButton(buttons.close, 'close', 'Close', action('close', this));
 		makeButton(buttons.maximize, 'maximize', 'Maximize', action('maximize', this));
-		if (buttons.maximize) this.dom.title.addEvent('dblclick', this.maximize.bind(this));
+		if (buttons.maximize == true) this.dom.titleBody.addEvent('dblclick', this.maximize.bind(this));
 		makeButton(buttons.minimize, 'minimize', 'Minimize', action(buttons.roll ? 'roll' : 'minimize', this));
 		makeButton(buttons.minimize, 'restore', 'Restore', action('minimize', this));
 		makeButton(buttons.menu, 'menu', 'Menu', action('openmenu', this));
